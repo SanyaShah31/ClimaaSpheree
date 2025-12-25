4e9bff5b2c837f1e8b7f6d003a315bec6eef8792
Sanya Shah <124649087+SanyaShah31@users.noreply.github.com>
2025-12-25 12:51:37 +0530

    Add files via upload

---
+++ climasphere2/app/src/main/java/com/example/climasphere2/network/WeatherApiService.kt
@@ -0,0 +1,21 @@
+package com.example.climasphere2.network
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApiService {
+
+    @GET("weather")
+    suspend fun getCurrentWeather(
+        @Query("q") city: String,
+        @Query("appid") apiKey: String,
+        @Query("units") units: String = "metric"
+    ): CurrentWeatherResponse
+
+    @GET("forecast")
+    suspend fun getFiveDayForecast(
+        @Query("q") city: String,
+        @Query("appid") apiKey: String,
+        @Query("units") units: String = "metric"
+    ): ForecastResponse
+}
